c06283c95cb902d501b4b6c1c40a2937ce5763d2
Me7atonin <sammyalex2006@gmail.com>
2024-10-10 13:59:14 -0700

    More Levels, HTP Menu

--- Seasons Project/Assets/SCRIPTS/Menu.cs
+++ Seasons Project/Assets/SCRIPTS/Menu.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField]
-    string leveltoload = "1stPart";
+    string leveltoload = "Spring 1";
     // Start is called before the first frame update
-  
+
 
     public void LoadNextInBuild()
-        {
+    {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+    }
 
     public void QuitGame()
     {
         Application.Quit();
     }
 
-}
+    public void HowToPlay()
+    {
+
+    }
+
+
+        }
