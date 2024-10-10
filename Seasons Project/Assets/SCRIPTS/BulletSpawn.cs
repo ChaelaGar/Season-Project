8bf87d21c3bbc39ecc639e6f22849cabd3cbb9db
Armani <137173487+LuminousArmani@users.noreply.github.com>
2024-10-10 11:50:08 -0700

    Fixed PlayerShooting and Working on BulletSpawn

--- Seasons Project/Assets/SCRIPTS/BulletSpawn.cs
+++ Seasons Project/Assets/SCRIPTS/BulletSpawn.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BulletSpawn : MonoBehaviour
 {
+    GameObject prefab;
+    [SerializeField]
+    float timer = 0;
+    [SerializeField]
+    float spawnDelay = 0.2f;
+    [SerializeField]
+    bool Spawned = true;
+    [SerializeField]
+    bool pickedUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        Vector3 bulletPack = transform.position;
+        bulletPack.z = 0;
+        bulletPack.Normalize();
+        if (Spawned == false && pickedUp == false && timer > spawnDelay)
+        {
+            GameObject bulletPack = Instantiate(prefab, transform.position, Quaternion.identity);
+        }
     }
 }
