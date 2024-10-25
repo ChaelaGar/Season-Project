1887585d2559d8e55e61c4da9e7cb8fdf6dd9214
Me7atonin <sammyalex2006@gmail.com>
2024-10-25 12:16:58 -0700

    UpdatedCredits

---
+++ Seasons Project/Assets/SCRIPTS/Credit script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creditscript : MonoBehaviour
+{
+    public float scrollSpeed = 40f;
+
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+}
